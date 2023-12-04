1b2f35bfc968ecc5a5be06e386978d343fc334d8
olzayy <Leka20.02@mail.ru>
2023-12-04 13:17:03 +0300

    wip

--- MusicBand/src/main/java/com/example/soafirst/SoaFirstApplication.java
+++ MusicBand/src/main/java/com/example/soafirst/SoaFirstApplication.java
@@ -2,8 +2,10 @@ package com.example.soafirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SoaFirstApplication {
 
     public static void main(String[] args) {
